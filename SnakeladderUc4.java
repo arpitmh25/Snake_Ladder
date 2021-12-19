29b6917fe34d009931f1bde2250aa2effa3832b7
Arpit <arpitmh23@gmail.com>
2021-12-19 22:54:14 +0530

    Implementing Uc4

---
+++ SnakeladderUc4.java
@@ -0,0 +1,38 @@
+class SnakeladderUc4 {
+	public static final int NoPlay = 0;
+	public static final int Ladder = 1;
+	public static final int Snake = 2;
+	public static final int Start = 0;
+	public static void main(String[] Args) {
+		int Position = 0;
+		System.out.println("Start Position:" +Start);
+		while(Position<=100){
+		int Roll = (int) (Math.floor(Math.random()*10)%6+1);
+		System.out.println("Roll dice Number:" +Roll);
+		int Condition = (int) (Math.floor(Math.random()*10)%3);
+		System.out.println("Condition Required:" +Condition);
+		switch(Condition) {
+			case NoPlay:
+			System.out.println("New Position:" +Position);
+			break;
+			case Ladder:
+			Position=Position+Roll;
+			System.out.println("New Position:" +Position);
+			break;
+			case Snake:
+			Position=Position-Roll;
+			if(Position>=0)
+			System.out.println("New Position:" +Position);
+		    else
+			Position = 0;
+			System.out.println("New Position:" +Position);
+			break;
+			default:
+		    System.out.println("Default Position");
+			break;
+			}
+		}
+	     	if(Position>=100)
+            System.out.println("Player Won Hurray!");
+	}
+}
